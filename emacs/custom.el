4fe5b02a3048de6cf3b81874bd495c9e8eabfc18
Martin Halder <martin.halder@gmail.com>
2013-06-20 14:10:36 +0200

    Custom faces

--- emacs/custom.el
+++ emacs/custom.el
@@ -208,3 +208,15 @@
 (set-face-attribute 'org-level-2 nil :height 100)
 (set-face-attribute 'org-column nil :height 100)
 ;; -------------------- set startup file
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("64b7be5703b90e05f7bc1f63a9f689a7c931626462697bea9476b397da194bd9" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
